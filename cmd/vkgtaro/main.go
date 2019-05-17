789beaec07dbe1c6c7cd920fce3b51ff8b32fece
Hidetatsu Yaginuma <ygnmhdtt@gmail.com>
2019-05-17 14:33:40 +0900

    use vkgtaro ascii art

--- cmd/vkgtaro/main.go
+++ cmd/vkgtaro/main.go
@@ -1,27 +1,7 @@
 package main
 
-import (
-	"flag"
-	"os"
-
-	"github.com/vkg/taro/vkgtaro"
-)
-
-var (
-	l = flag.Bool("little", false, "Little")
-	s = flag.Bool("slow", false, "Slow")
-)
+import "github.com/vkg/taro/vkgtaro"
 
 func main() {
-	flag.Parse()
-	vkgtaro := vkgtaro.New(os.Stdout)
-	if *l {
-		vkgtaro.MoveLittle()
-		return
-	}
-	if *s {
-		vkgtaro.MoveSlow()
-		return
-	}
-	vkgtaro.Move()
+	vkgtaro.New().Move()
 }
--- vkgtaro/vkgtaro.go
+++ vkgtaro/vkgtaro.go
@@ -1,37 +1,9 @@
 package vkgtaro
 
 import (
-	"fmt"
-	"io"
-	"time"
-)
+	"os"
 
-const (
-	escapeSequenceFormat = "\x1b[%d;%dH\x1bK"
-
-	printDurationVertical       = 30 * time.Millisecond
-	printDurationHorizontal     = 0 * time.Millisecond
-	printDurationHorizontalSlow = 30 * time.Millisecond
-)
-
-var (
-	vkgtaroLines = []string{
-		"         ##                                                   ",
-		"          #                  #                                ",
-		"          #                  #                                ",
-		"          #                  #                                ",
-		"##   ##   #    #    ### ## ######     ####   ### ##     ###   ",
-		"#     #   #   #    #   #     #       #    #    ##  #   #   #  ",
-		"#     #   #  #     #   #     #            #    #   #  #     # ",
-		" #   #    # ##     #   #     #        #####    #      #     # ",
-		" #   #    ##  #     ###      #       #    #    #      #     # ",
-		" #   #    #   #    #         #      #     #    #      #     # ",
-		"  # #     #    #   ####      #   #  #     #    #      #     # ",
-		"  # #     #    #  #    #     #   #  #    ##    #       #   #  ",
-		"   #     ###   ## #     #     ###    #### ## ######     ###   ",
-		"                  #     #                                     ",
-		"                   #####                                      ",
-	}
+	"github.com/vkg/taro/printer"
 )
 
 var _ Vkgtaro = (*vkgtaroImpl)(nil)
@@ -39,65 +11,19 @@ var _ Vkgtaro = (*vkgtaroImpl)(nil)
 // Vkgtaro is vkg service
 type Vkgtaro interface {
 	Move()
-	MoveLittle()
-	MoveSlow()
 }
 
 type vkgtaroImpl struct {
-	out io.Writer
+	printer *printer.Printer
 }
 
 // New returns vkgtaro
-func New(out io.Writer) Vkgtaro {
-	return &vkgtaroImpl{out: out}
+func New() Vkgtaro {
+	printer := printer.New(os.Stdout)
+	return &vkgtaroImpl{printer: printer}
 }
 
-// Move moves normally
+// Move starts moving
 func (v *vkgtaroImpl) Move() {
-	v.clear()
-	for y := 80; y > 0; y-- {
-		for x := 1; x <= 15; x++ {
-			fmt.Fprintf(v.out, escapeSequenceFormat, x, y)
-			fmt.Println(vkgtaroLine(x))
-			time.Sleep(printDurationHorizontal)
-		}
-		time.Sleep(printDurationVertical)
-	}
-	v.clear()
-}
-
-// MoveLittle little version
-func (v *vkgtaroImpl) MoveLittle() {
-	v.clear()
-	for y := 80; y > 0; y-- {
-		fmt.Fprintf(v.out, escapeSequenceFormat, 15, y)
-		fmt.Println("vkgtaro ") // last space is necessary
-		time.Sleep(printDurationVertical)
-	}
-	v.clear()
-}
-
-// MoveSlow slowly
-func (v *vkgtaroImpl) MoveSlow() {
-	v.clear()
-	for y := 80; y > 0; y-- {
-		for x := 1; x <= 15; x++ {
-			fmt.Fprintf(v.out, escapeSequenceFormat, x, y)
-			fmt.Println(vkgtaroLine(x))
-			time.Sleep(printDurationHorizontalSlow)
-		}
-		time.Sleep(printDurationVertical)
-	}
-	v.clear()
-}
-
-func (v *vkgtaroImpl) clear() {
-	fmt.Fprintln(v.out, "\x1b[2J")
-}
-
-func vkgtaroLine(line int) string {
-	if line < 1 || 15 < line {
-		return ""
-	}
-	return vkgtaroLines[line-1]
+	v.printer.PrintAndMove(printer.NewSource(photo))
 }
